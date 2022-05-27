ccd636a045fcf61fef4d4668d3be31cf6e7b59ea
ebiapollos <doyinapollos@gmail.com>
2022-05-28 00:01:55 +0100

    create-acct-compt-1

--- src/index.js
+++ src/index.js
@@ -2,11 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { configureStore } from '@reduxjs/toolkit';
+import { Provider } from 'react-redux';
+import productRducer from './features/productSlice';
+
+
+
+
+const store = configureStore({
+  reducer: {
+    products: productRducer,
+  },
+})
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render( 
       <React.StrictMode>
-         <App/>
+          <Provider store={store}>
+              <App/>
+          </Provider>
+         
     </React.StrictMode>
 );
